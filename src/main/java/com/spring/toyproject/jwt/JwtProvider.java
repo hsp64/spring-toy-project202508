3ba7d8c49f9018de939165e78f0b26879524dbd3
hsp64 <pphs604@gmail.com>
2025-08-07 11:10:33 +0900

    로그인 API 응답 개선 - AuthResponse DTO 추가 및 JWT 토큰 반환 로직 구현, UserService의 authenticate 메서드 업데이트, AuthController에서 로그인 응답 데이터 반환하도록 수정 페이지 렌더링을 위한 컨트롤러 및 뷰 구현 - PageController 생성, index.html 수정, login.html 및 signup.html 추가, 회원가입 페이지 스타일링용 signup.css 추가

--- src/main/java/com/spring/toyproject/jwt/JwtProvider.java
+++ src/main/java/com/spring/toyproject/jwt/JwtProvider.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
-import java.util.Properties;
 
 /**
  * JWT 토큰 생성, 검증, 파싱 기능 제공 유틸클래스
@@ -25,24 +24,25 @@ public class JwtProvider {
     /**
      * JWT 토큰을 발급하는 메서드
      * @param username - 발급대상의 사용자 이름 (유일하게 사용자를 식별할 값)
-     * @return
+     * @return - JWT 토큰 문자열 (암호화됨)
      */
     public String generateToken(String username) {
 
         Date now = new Date();
-        Date expiryDate = new Date(now.getTime() + jwtProperties.getExpiration() );
+        Date expiryDate = new Date(now.getTime() + jwtProperties.getExpiration());
 
         return Jwts.builder()
                 .subject(username) // 이 토큰을 유일하게 식별할 키
-                .issuedAt(expiryDate) // 언제 발급했는지
-                .issuer("Toy project By KDT") // 발급자 정보
-                .signWith(getSigningKey()) // 서명
+                .issuedAt(now) // 언제 발급했는지
+                .expiration(expiryDate) // 언제 만료되는지
+                .issuer("Toy Project By KDT") // 발급자 정보
+                .signWith(getSigningKey())   // 서명
                 .compact();
     }
 
     /**
-     *  JWT 토큰 발급에 필욯나 서명 만들기
-     *  return 서명 키 객체
+     * JWT 토큰 발급에 필요한 서명 만들기
+     * @return 서명 키 객체
      */
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8));
